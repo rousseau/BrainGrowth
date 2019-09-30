6c9f941aaf73fd380eb3f3636de40be9fbfae759
x17wang <40798933+x17wang@users.noreply.github.com>
2019-09-30 16:32:35 +0200

    Update geometry.py

--- geometry.py
+++ geometry.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 from numba import jit, njit, prange
-from mathfunc import det_dim_3, det_dim_2, cross_dim_3, dot_mat_dim_3, transpose_dim_3, normalize_dim_3, dot_const_mat_dim_3
+from mathfunc import det_dim_3, det_dim_2, cross_dim_3, dot_mat_dim_3, transpose_dim_3, normalize_dim_3
 
 # Import mesh, each line as a list
 def importMesh(path):
