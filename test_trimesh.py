bc7189966729f23b345c363fb787a2a171e92fb2
x17wang <40798933+x17wang@users.noreply.github.com>
2019-10-13 19:52:46 +0200

    Update test_trimesh.py

--- test_trimesh.py
+++ test_trimesh.py
@@ -4,9 +4,10 @@ import numpy as np
 #From https://github.com/MahsaShk/MeshProcessing
 from nii_2_mesh_conversion import nii_2_mesh
 
-# Path of input image and output mesh
-filename_nii = '/home/x17wang/Exp/prm001/prm001_30w_Lwhite_r05.nii.gz'
-filename_stl = '/home/x17wang/Exp/prm001/prm001_30w_Lwhite_r05.stl'
+
+filename_nii = '/home/x17wang/Exp/prm001/prm001_30w_Rwhite.nii'
+filename_nii_reso = '/home/x17wang/Exp/prm001/prm001_30w_Rwhite_r05.nii.gz'
+filename_stl = '/home/x17wang/Exp/prm001/prm001_30w_Rwhite_r05.stl'
 
 #Remove directions
 arr = nib.load(filename_nii).get_data()
@@ -15,18 +16,20 @@ mat = np.eye(4)
 mat[0,0] = reso
 mat[1,1] = reso
 mat[2,2] = reso
-nib.save(nib.Nifti1Image(arr,mat), filename_nii)
+nib.save(nib.Nifti1Image(arr,mat), filename_nii_reso)
+
 
 ## Transform nifti image to stl mesh
 label = 0
-nii_2_mesh(filename_nii, filename_stl, label)
+nii_2_mesh(filename_nii_reso, filename_stl, label)
 
 ## Transform stl mesh to nifti image
 # Path of input mesh
 #inputFIlePath = '/home/x17wang/Exp/prm001/prm001_40w_Rwhite.stl'
 
 # Load stl mesh
-m = trimesh.load(filename_stl) 
+#filename_stl_simu = '/home/x17wang/Codes/BrainGrowth/res/prm001_30w_Rwhite_r05_smooth_2/pov_H0.042000AT1.829000/B0.stl'
+m = trimesh.load(filename_stl)
 
 # Voxelize mesh with the specific edge length of a single voxel
 v = m.voxelized(pitch=0.25)  #Return a Voxel object representing the current mesh discretized into voxels at the specified pitch, pitch: float, the edge length of a single voxel
@@ -36,12 +39,13 @@ v = v.fill(method='holes')
 
 # Center coordinates
 cog = np.sum(v.points, axis=0)
-cog /= np.size(v.points, axis=0) 
+cog /= np.size(v.points, axis=0)
 
 # Convert mesh to binary image
 #print(cog)
 #outimage = np.zeros((2*int(np.round(cog[0]/reso))+1, 2*int(np.round(cog[1]/reso))+1, 2*int(np.round(cog[2]/reso))+1), dtype=np.int16)
-outimage = np.zeros(arr.shape)
+arr_reso = nib.load(filename_nii_reso).get_data()
+outimage = np.zeros(arr_reso.shape)
 print(outimage.shape)
 
 for i in range(np.size(v.points, axis=0)):
@@ -52,7 +56,7 @@ for i in range(np.size(v.points, axis=0)):
 
 # Save binary image in a nifti file
 #nii = nib.load('/home/x17wang/Data/prm001/prm001_40w_Rwhite.nii')
-save_path = '/home/x17wang/Exp/prm001/test_prm001_30w_Lwhite_r05.nii.gz'
+save_path = '/home/x17wang/Exp/prm001/test_prm001_30w_Rwhite_r05.nii.gz' #'/home/x17wang/Codes/BrainGrowth/res/prm001_30w_Rwhite_r05_smooth_2/pov_H0.042000AT1.829000/B0.nii.gz'  
 
 #img = nib.Nifti1Image(outimage, nii.affine)
 aff = np.eye(4)
@@ -62,3 +66,12 @@ aff[2,2] = reso
 
 img = nib.Nifti1Image(outimage, aff)
 nib.save(img, save_path)
+
+# Volume mesh generation using Gmsh SDK
+#filename_msh = '/home/x17wang/Exp/prm001/prm001_30w_Rwhite_r05_2.msh'
+#p = trimesh.smoothing.filter_laplacian(m, lamb=0.5, iterations=10, implicit_time_integration=False, volume_constraint=True, laplacian_operator=None)
+#m = trimesh.repair.fill_holes(m)
+#m = trimesh.repair.fix_winding(m)
+#m = trimesh.repair.fix_normals(m, multibody=False)
+#m = trimesh.smoothing.filter_laplacian(m, lamb=0.5, iterations=10, implicit_time_integration=False, volume_constraint=True, laplacian_operator=None)
+#trimesh.interfaces.gmsh.to_volume(m, filename_msh, max_element=0.01, mesher_id=1)
