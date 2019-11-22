74c465a43d9c393b6ae9bc801873effe4da1cfec
x17wang <40798933+x17wang@users.noreply.github.com>
2019-11-22 15:07:51 +0100

    Update normalisation.py

--- normalisation.py
+++ normalisation.py
@@ -27,9 +27,9 @@ def normalise_coord(Ut0, Ut, nn):
   #maxd = max(max(max(abs(maxx-cog[0]), abs(minx-cog[0])), abs(maxy-miny)), max(abs(maxz-cog[2]), abs(minz-cog[2])))  # The biggest value of difference between the coordinate(x, y, z) and center(x, y,z) respectively
   #maxd = max(abs(maxx-minx), abs(maxy-miny), abs(maxz-minz))
 
-  Ut0[:,0] = (Ut[:,0] - cog[0])/maxd
+  Ut0[:,0] = -(Ut[:,0] - cog[0])/maxd
   Ut0[:,1] = (Ut[:,1] - cog[1])/maxd
-  Ut0[:,2] = (Ut[:,2] - cog[2])/maxd
+  Ut0[:,2] = -(Ut[:,2] - cog[2])/maxd
 
   """Ut0[:,0] = -(Ut[:,0] - cog[0])/maxd
   Ut0[:,1] = (Ut[:,1] - miny)/maxd
@@ -39,4 +39,4 @@ def normalise_coord(Ut0, Ut, nn):
 
   Ut = Ut0
 
-  return Ut0, Ut, cog, maxd
+  return Ut0, Ut, cog, maxd, miny
