8af727304843ba4fe2baef2db111cc7cfd1a97c5
x17wang <40798933+x17wang@users.noreply.github.com>
2020-01-23 14:38:16 +0100

    Update normalisation.py

--- normalisation.py
+++ normalisation.py
@@ -24,19 +24,23 @@ def normalise_coord(Ut0, Ut, nn, halforwholebrain):
 
   # Change mesh information by values normalized 
   #maxd = max(max(max(abs(maxx-cog[0]), abs(minx-cog[0])), max(abs(maxy-cog[1]), abs(miny-cog[1]))), max(abs(maxz-cog[2]), abs(minz-cog[2])))
-  maxd = max(max(max(abs(maxx-cog[0]), abs(minx-cog[0])), abs(maxy-miny)), max(abs(maxz-cog[2]), abs(minz-cog[2])))  # The biggest value of difference between the coordinate(x, y, z) and center(x, y,z) respectively
+  #maxd = max(max(max(abs(maxx-cog[0]), abs(minx-cog[0])), abs(maxy-miny)), max(abs(maxz-cog[2]), abs(minz-cog[2])))  # The biggest value of difference between the coordinate(x, y, z) and center(x, y,z) respectively
   #maxd = max(abs(maxx-minx), abs(maxy-miny), abs(maxz-minz))
 
   """Ut0[:,0] = (Ut[:,0] - cog[0])/maxd
   Ut0[:,1] = (Ut[:,1] - cog[1])/maxd
   Ut0[:,2] = (Ut[:,2] - cog[2])/maxd"""
   
-  Ut0[:,0] = -(Ut[:,0] - cog[0])/maxd
   if halforwholebrain.__eq__("half"):
+    maxd = max(max(max(abs(maxx-cog[0]), abs(minx-cog[0])), abs(maxy-miny)), max(abs(maxz-cog[2]), abs(minz-cog[2])))
+    Ut0[:,0] = -(Ut[:,0] - cog[0])/maxd
     Ut0[:,1] = (Ut[:,1] - miny)/maxd
+    Ut0[:,2] = -(Ut[:,2] - cog[2])/maxd
   else:
+    maxd = max(max(max(abs(maxx-cog[0]), abs(minx-cog[0])), max(abs(maxy-cog[1]), abs(miny-cog[1]))), max(abs(maxz-cog[2]), abs(minz-cog[2])))
+    Ut0[:,0] = -(Ut[:,0] - cog[0])/maxd
     Ut0[:,1] = (Ut[:,1] - cog[1])/maxd
-  Ut0[:,2] = -(Ut[:,2] - cog[2])/maxd
+    Ut0[:,2] = -(Ut[:,2] - cog[2])/maxd
 
   print ('normalized minx is ' + str(min(Ut0[:,0])) + ' normalized maxx is ' + str(max(Ut0[:,0])) + ' normalized miny is ' + str(min(Ut0[:,1])) + ' normalized maxy is ' + str(max(Ut0[:,1])) + ' normalized minz is ' + str(min(Ut0[:,2])) + ' normalized maxz is ' + str(max(Ut0[:,2])))
 
