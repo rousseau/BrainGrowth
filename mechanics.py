07b95301bb1fef13c2718918a68dcda6eea885ed
Benjamin-Fouquet <fouquet.benjamin@live.fr>
2021-06-23 10:52:11 +0200

    tab format coherence

--- mechanics.py
+++ mechanics.py
@@ -121,102 +121,105 @@ def tetraElasticity_leg(material_tets, ref_state_tets, Ft, tan_growth_tensor, bu
 #updated version for last numba, small deviation of Ft at ~7th decimale against tetraElasticity
 @jit(nopython=True, parallel=True)   
 def tetra_elasticity(material_tets, ref_state_tets, Ft, tan_growth_tensor, bulk_modulus, k_param, mu, tets, Vn, Vn0, n_tets, eps):
+  """
+  Calculates elastic forces
+  """
     
-    #tetraelasticty variables
-    ref_state_growth = np.zeros ((n_tets, 3, 3), dtype=np.float64) #Ar
-    deformation_grad = np.zeros((n_tets,3,3), dtype=np.float64)
-    left_cauchy_grad = np.zeros((n_tets,3,3), dtype=np.float64)
-    rel_vol_chg = np.zeros(n_tets, dtype=np.float64)
-    rel_vol_chg1 = np.zeros(n_tets, dtype=np.float64)
-    rel_vol_chg2 = np.zeros(n_tets, dtype=np.float64)
-    rel_vol_chg3 = np.zeros(n_tets, dtype=np.float64)
-    rel_vol_chg4 = np.zeros(n_tets, dtype=np.float64)
-    rel_vol_chg_av = np.zeros(n_tets, dtype=np.float64)
+  #tetraelasticty variables
+  ref_state_growth = np.zeros ((n_tets, 3, 3), dtype=np.float64) #Ar
+  deformation_grad = np.zeros((n_tets,3,3), dtype=np.float64)
+  left_cauchy_grad = np.zeros((n_tets,3,3), dtype=np.float64)
+  rel_vol_chg = np.zeros(n_tets, dtype=np.float64)
+  rel_vol_chg1 = np.zeros(n_tets, dtype=np.float64)
+  rel_vol_chg2 = np.zeros(n_tets, dtype=np.float64)
+  rel_vol_chg3 = np.zeros(n_tets, dtype=np.float64)
+  rel_vol_chg4 = np.zeros(n_tets, dtype=np.float64)
+  rel_vol_chg_av = np.zeros(n_tets, dtype=np.float64)
     
-    #Apply growth to reference state
-    ref_state_growth = dot_mat_dim_3(tan_growth_tensor, ref_state_tets)
-    #Calculate deformation gradient F //combine relative volume change ?
-    deformation_grad = dot_mat_dim_3(material_tets, inv_dim_3(ref_state_growth))   
-    #Calculate Left-Cauchy-Green gradient B
-    left_cauchy_grad = dot_mat_dim_3(deformation_grad, np.transpose(deformation_grad, (0, 2, 1)))
-    #relative volume change J
-    rel_vol_chg = det_dim_3(deformation_grad)
-    #averaged volume change
-    rel_vol_chg1 = Vn[tets[:,0]]/Vn0[tets[:,0]]
-    rel_vol_chg2 = Vn[tets[:,1]]/Vn0[tets[:,1]]
-    rel_vol_chg3 = Vn[tets[:,2]]/Vn0[tets[:,2]]
-    rel_vol_chg4 = Vn[tets[:,3]]/Vn0[tets[:,3]]
-    rel_vol_chg_av = (rel_vol_chg1 + rel_vol_chg2 + rel_vol_chg3 + rel_vol_chg4)/4.0   
+  #Apply growth to reference state
+  ref_state_growth = dot_mat_dim_3(tan_growth_tensor, ref_state_tets)
+  #Calculate deformation gradient F //combine relative volume change ?
+  deformation_grad = dot_mat_dim_3(material_tets, inv_dim_3(ref_state_growth))   
+  #Calculate Left-Cauchy-Green gradient B
+  left_cauchy_grad = dot_mat_dim_3(deformation_grad, np.transpose(deformation_grad, (0, 2, 1)))
+  #relative volume change J
+  rel_vol_chg = det_dim_3(deformation_grad)
+  #averaged volume change
+  rel_vol_chg1 = Vn[tets[:,0]]/Vn0[tets[:,0]]
+  rel_vol_chg2 = Vn[tets[:,1]]/Vn0[tets[:,1]]
+  rel_vol_chg3 = Vn[tets[:,2]]/Vn0[tets[:,2]]
+  rel_vol_chg4 = Vn[tets[:,3]]/Vn0[tets[:,3]]
+  rel_vol_chg_av = (rel_vol_chg1 + rel_vol_chg2 + rel_vol_chg3 + rel_vol_chg4)/4.0   
     
-    #decide if need SVD or not
-    for i in prange (n_tets):
+  #decide if need SVD or not
+  for i in prange (n_tets):
         
-        ll1, ll2, ll3 = EV(left_cauchy_grad[i])
+    ll1, ll2, ll3 = EV(left_cauchy_grad[i])
         
-        if ll3 >= eps*eps and rel_vol_chg[i] > 0.0:  # No need for SVD
-            powJ23 = np.power(rel_vol_chg[i], 2.0/3.0)
-            S = (left_cauchy_grad[i] - np.identity(3)*np.trace(left_cauchy_grad[i])/3.0)*mu[i]/(rel_vol_chg[i]*powJ23) + np.identity(3)*bulk_modulus*(rel_vol_chg_av[i]-1.0)
-            P = np.dot(S, np.linalg.inv(deformation_grad[i].transpose()))*rel_vol_chg[i]
-            W = 0.5*mu[i]*(np.trace(left_cauchy_grad[i])/powJ23 - 3.0) + 0.5*bulk_modulus*((rel_vol_chg1[i]-1.0)*(rel_vol_chg1[i]-1.0) + (rel_vol_chg2[i]-1.0)*(rel_vol_chg2[i]-1.0) + (rel_vol_chg3[i]-1.0)*(rel_vol_chg3[i]-1.0) + (rel_vol_chg4[i]-1.0)*(rel_vol_chg4[i]-1.0))*0.25
+    if ll3 >= eps*eps and rel_vol_chg[i] > 0.0:  # No need for SVD
+      powJ23 = np.power(rel_vol_chg[i], 2.0/3.0)
+      S = (left_cauchy_grad[i] - np.identity(3)*np.trace(left_cauchy_grad[i])/3.0)*mu[i]/(rel_vol_chg[i]*powJ23) + np.identity(3)*bulk_modulus*(rel_vol_chg_av[i]-1.0)
+      P = np.dot(S, np.linalg.inv(deformation_grad[i].transpose()))*rel_vol_chg[i]
+      W = 0.5*mu[i]*(np.trace(left_cauchy_grad[i])/powJ23 - 3.0) + 0.5*bulk_modulus*((rel_vol_chg1[i]-1.0)*(rel_vol_chg1[i]-1.0) + (rel_vol_chg2[i]-1.0)*(rel_vol_chg2[i]-1.0) + (rel_vol_chg3[i]-1.0)*(rel_vol_chg3[i]-1.0) + (rel_vol_chg4[i]-1.0)*(rel_vol_chg4[i]-1.0))*0.25
         
         
-        else:   #need SVD
-            C = np.dot(deformation_grad[i].transpose(), deformation_grad[i])
-            w2, v2 = np.linalg.eigh(C)
-            v2 = -v2
+    else:   #need SVD
+      C = np.dot(deformation_grad[i].transpose(), deformation_grad[i])
+      w2, v2 = np.linalg.eigh(C)
+      v2 = -v2
     
-            l1 = sqrt(w2[0])
-            l2 = sqrt(w2[1])
-            l3 = sqrt(w2[2])
+      l1 = sqrt(w2[0])
+      l2 = sqrt(w2[1])
+      l3 = sqrt(w2[2])
     
-            if np.linalg.det(v2) < 0.0:
-                v2[0,0] = -v2[0,0]
-                v2[1,0] = -v2[1,0]
-                v2[2,0] = -v2[2,0]
+      if np.linalg.det(v2) < 0.0:
+        v2[0,0] = -v2[0,0]
+        v2[1,0] = -v2[1,0]
+        v2[2,0] = -v2[2,0]
     
-            Fdi = np.identity(3)
-            if l1 >= 1e-25:
-                Fdi[0,0] = 1.0/l1
-                Fdi[1,1] = 1.0/l2
-                Fdi[2,2] = 1.0/l3
+      Fdi = np.identity(3)
+      if l1 >= 1e-25:
+        Fdi[0,0] = 1.0/l1
+        Fdi[1,1] = 1.0/l2
+        Fdi[2,2] = 1.0/l3
     
-            U = np.dot(deformation_grad[i], np.dot(v2, Fdi))
+      U = np.dot(deformation_grad[i], np.dot(v2, Fdi))
     
-            if l1 < 1e-25:
-                U[0,0] = U[1,1]*U[2,2] - U[2,1]*U[1,2]
-                U[1,0] = U[2,1]*U[0,2] - U[0,1]*U[2,2]
-                U[2,0] = U[0,1]*U[1,2] - U[1,1]*U[0,2]
+      if l1 < 1e-25:
+        U[0,0] = U[1,1]*U[2,2] - U[2,1]*U[1,2]
+        U[1,0] = U[2,1]*U[0,2] - U[0,1]*U[2,2]
+        U[2,0] = U[0,1]*U[1,2] - U[1,1]*U[0,2]
     
-            if np.linalg.det(deformation_grad[i]) < 0.0:
-                l1 = -l1
-                U[0,0] = -U[0,0]
-                U[1,0] = -U[1,0]
-                U[2,0] = -U[2,0]
+      if np.linalg.det(deformation_grad[i]) < 0.0:
+        l1 = -l1
+        U[0,0] = -U[0,0]
+        U[1,0] = -U[1,0]
+        U[2,0] = -U[2,0]
     
-            Pd = np.identity(3)
-            pow23 = np.power(eps*l2*l3, 2.0/3.0)
-            Pd[0,0] = mu[i]/3.0*(2.0*eps - l2*l2/eps - l3*l3/eps)/pow23 + k_param*(l1-eps) + bulk_modulus*(rel_vol_chg_av[i]-1.0)*l2*l3
-            Pd[1,1] = mu[i]/3.0*(-eps*eps/l2 + 2.0*l2 - l3*l3/l2)/pow23 + mu[i]/9.0*(-4.0*eps/l2 - 4.0/eps*l2 + 2.0/eps/l2*l3*l3)/pow23*(l1-eps) + bulk_modulus*(rel_vol_chg_av[i]-1.0)*l1*l3
-            Pd[2,2] = mu[i]/3.0*(-eps*eps/l3 - l2*l2/l3 + 2.0*l3)/pow23 + mu[i]/9.0*(-4.0*eps/l3 + 2.0/eps*l2*l2/l3 - 4.0/eps*l3)/pow23*(l1-eps) + bulk_modulus*(rel_vol_chg_av[i]-1.0)*l1*l2
-            P = np.dot(U, np.dot(Pd, v2.transpose()))
-            W = 0.5*mu[i]*((eps*eps + l2*l2 + l3*l3)/pow23 - 3.0) + mu[i]/3.0*(2.0*eps - l2*l2/eps - l3*l3/eps)/pow23*(l1-eps) + 0.5*k_param*(l1-eps)*(l1-eps) + 0.5*bulk_modulus*((rel_vol_chg1[i]-1.0)*(rel_vol_chg1[i]-1.0) + (rel_vol_chg2[i]-1.0)*(rel_vol_chg2[i]-1.0) + (rel_vol_chg3[i]-1.0)*(rel_vol_chg3[i]-1.0) + (rel_vol_chg4[i]-1.0)*(rel_vol_chg4[i]-1.0))/4.0
+      Pd = np.identity(3)
+      pow23 = np.power(eps*l2*l3, 2.0/3.0)
+      Pd[0,0] = mu[i]/3.0*(2.0*eps - l2*l2/eps - l3*l3/eps)/pow23 + k_param*(l1-eps) + bulk_modulus*(rel_vol_chg_av[i]-1.0)*l2*l3
+      Pd[1,1] = mu[i]/3.0*(-eps*eps/l2 + 2.0*l2 - l3*l3/l2)/pow23 + mu[i]/9.0*(-4.0*eps/l2 - 4.0/eps*l2 + 2.0/eps/l2*l3*l3)/pow23*(l1-eps) + bulk_modulus*(rel_vol_chg_av[i]-1.0)*l1*l3
+      Pd[2,2] = mu[i]/3.0*(-eps*eps/l3 - l2*l2/l3 + 2.0*l3)/pow23 + mu[i]/9.0*(-4.0*eps/l3 + 2.0/eps*l2*l2/l3 - 4.0/eps*l3)/pow23*(l1-eps) + bulk_modulus*(rel_vol_chg_av[i]-1.0)*l1*l2
+      P = np.dot(U, np.dot(Pd, v2.transpose()))
+      #W = 0.5*mu[i]*((eps*eps + l2*l2 + l3*l3)/pow23 - 3.0) + mu[i]/3.0*(2.0*eps - l2*l2/eps - l3*l3/eps)/pow23*(l1-eps) + 0.5*k_param*(l1-eps)*(l1-eps) + 0.5*bulk_modulus*((rel_vol_chg1[i]-1.0)*(rel_vol_chg1[i]-1.0) + (rel_vol_chg2[i]-1.0)*(rel_vol_chg2[i]-1.0) + (rel_vol_chg3[i]-1.0)*(rel_vol_chg3[i]-1.0) + (rel_vol_chg4[i]-1.0)*(rel_vol_chg4[i]-1.0))/4.0
      
-        # Calculate tetra face negative normals (because traction Ft=-P*n)
-        xr1 = np.array([ref_state_growth[i,0,0], ref_state_growth[i,1,0], ref_state_growth[i,2,0]])
-        xr2 = np.array([ref_state_growth[i,0,1], ref_state_growth[i,1,1], ref_state_growth[i,2,1]])
-        xr3 = np.array([ref_state_growth[i,0,2], ref_state_growth[i,1,2], ref_state_growth[i,2,2]])
-        N1 = np.cross(xr3, xr1)
-        N2 = np.cross(xr2, xr3)
-        N3 = np.cross(xr1, xr2)
-        N4 = np.cross(xr2-xr3, xr1-xr3)
+    # Calculate tetra face negative normals (because traction Ft=-P*n)
+    xr1 = np.array([ref_state_growth[i,0,0], ref_state_growth[i,1,0], ref_state_growth[i,2,0]])
+    xr2 = np.array([ref_state_growth[i,0,1], ref_state_growth[i,1,1], ref_state_growth[i,2,1]])
+    xr3 = np.array([ref_state_growth[i,0,2], ref_state_growth[i,1,2], ref_state_growth[i,2,2]])
+    N1 = np.cross(xr3, xr1)
+    N2 = np.cross(xr2, xr3)
+    N3 = np.cross(xr1, xr2)
+    N4 = np.cross(xr2-xr3, xr1-xr3)
         
-        # Distribute forces among tetra vertices, probably not vectorizable. Surprising that its //
-        Ft[tets[i,0]] += np.dot(P, (N1 + N2 + N3).T)/6.0
-        Ft[tets[i,1]] += np.dot(P, (N1 + N3 + N4).T)/6.0
-        Ft[tets[i,2]] += np.dot(P, (N2 + N3 + N4).T)/6.0
-        Ft[tets[i,3]] += np.dot(P, (N1 + N2 + N4).T)/6.0
+      # Distribute forces among tetra vertices, probably not vectorizable. Surprising that its //
+    Ft[tets[i,0]] += np.dot(P, (N1 + N2 + N3).T)/6.0
+    Ft[tets[i,1]] += np.dot(P, (N1 + N3 + N4).T)/6.0
+    Ft[tets[i,2]] += np.dot(P, (N2 + N3 + N4).T)/6.0
+    Ft[tets[i,3]] += np.dot(P, (N1 + N2 + N4).T)/6.0
         
-    return Ft
+  return Ft
 
 #pure np/python version
 def tetraElasticity_np(material_tets, ref_state_tets, Ft, tan_growth_tensor, bulk_modulus, k_param, mu, tets, Vn, Vn0, n_tets, eps):
